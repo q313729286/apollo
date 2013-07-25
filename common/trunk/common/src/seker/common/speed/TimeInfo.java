18a04eae483edff9f5bafe9fd410067fd3201811
seker <seker2009@gmail.com>
2013-07-25 19:46:04 +0800

    标准化TimeLogger类
    Signed-off-by: seker <seker2009@gmail.com>

--- common/trunk/common/src/seker/common/speed/TimeInfo.java
+++ common/trunk/common/src/seker/common/speed/TimeInfo.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2012 Baidu Inc. All rights reserved.
+ */
 package seker.common.speed;
 
 /**
@@ -6,28 +9,35 @@ package seker.common.speed;
  * @author Liu Xinjian
  */
 class TimeInfo {
-    /** 模块TAG */
-    final String tag;
-
+    
     /** 运行的系统时间点 */
     final long time;
-
+    
     /** 运行的线程ID */
     final long thread;
+    
+    /** 模块TAG */
+    final String model;
+    
+    /** 模块TAG */
+    final String tag;
 
     /**
      * 时间点信息
      * 
-     * @param ta
-     *            模块TAG
      * @param ti
      *            运行的系统时间点
      * @param th
      *            运行的线程ID
+     * @param md
+     *            模块名称
+     * @param ta
+     *            模块TAG
      */
-    public TimeInfo(String ta, long ti, long th) {
-        tag = ta;
+    public TimeInfo(long ti, long th, String md, String ta) {
         time = ti;
         thread = th;
+        model = md;
+        tag = ta;
     }
 }
